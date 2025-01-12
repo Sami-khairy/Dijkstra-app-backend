36fcf9d197bdd5a507fa913b3c3f6d9b031c0a76
Sami-Khairy <samikhairy2003@gmail.com>
2025-01-13 00:35:11 +0100

    create controller

---
+++ src/main/java/ma/khairy/backend/Controller/DijkstraController.java
@@ -0,0 +1,26 @@
+package ma.khairy.backend.Controller;
+
+import ma.khairy.backend.Model.Graphe;
+import ma.khairy.backend.Service.DijkstraService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/dijkstra")
+public class DijkstraController {
+
+    private final DijkstraService dijkstraService;
+
+    public DijkstraController(DijkstraService dijkstraService) {
+        this.dijkstraService = dijkstraService;
+    }
+
+    @PostMapping("/calculate")
+    public Map<String, Integer> calculateShortestPath(
+            @RequestParam String startNode,
+            @RequestParam(required = false, defaultValue = "false") boolean details,
+            @RequestBody Graphe graphe) {
+        return dijkstraService.calculerCheminMinimal(graphe, startNode, details);
+    }
+}
